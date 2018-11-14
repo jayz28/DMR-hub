e1c22af09231d062d04fef88768235094cfb22a4
Jay <jay@resolutionbio.com>
2018-11-14 15:21:23 -0800

    add UDP client

--- src/ambe72_to_ambe49_DSD.py
+++ src/ambe72_to_ambe49_DSD.py
@@ -5,9 +5,9 @@ from dmr_utils import ambe_utils
 from bitarray import bitarray
 from time import time, sleep
 from multiprocessing import Pool, Process, Pipe
-from time import time, sleep
 from io import BytesIO
 from functools import partial
+from socket import socket, AF_INET, SOCK_DGRAM  #, MSG_DONTWAIT
 
 
 def main():
@@ -15,33 +15,40 @@ def main():
     tones = ''
     with open('769.bin.ambe', 'rt') as dtmf:
         for line in dtmf:
-            tones += '0'*8 + line.rstrip()
-
+            tones += '0'*8 + line.rstrip() + '0'*7
+    
     dtmf_tones = bitarray(tones)
 
     data_buffer = b'' 
     silent = bitarray('000000001111100000000001101010011001111110001100111000001')
 
     sys.stdout.buffer.write(b'.amb')
-    sys.stdout.buffer.write(silent.tobytes())
+    sys.stdout.buffer.write(silent.tobytes()*10 + dtmf_tones.tobytes())
 
     pipe = Pipe()
     p = Process(target=output_process, args=(pipe[0], dtmf_tones))
     p.start()
-   
+
+    sock = socket(AF_INET, SOCK_DGRAM)
+    sock.bind(('127.0.0.1', 31000))
+
     sys.stdout.buffer.flush()
 
+    data = b''
     with Pool(3) as p:
         while True:
-            data = sys.stdin.buffer.read(9*6)
+            # data = sys.stdin.buffer.read(9*6)
+            #try:
+            data = sock.recv(9*6) #, MSG_DONTWAIT)
+            #except:
+                #pass
+
             if data:
                 data_buffer += data
                 if len(data_buffer) > 8:  # need 9 bytes for one ambe72 frame
                     io_data = BytesIO(data_buffer)
-                    ambe72_frames = [frame for frame in iter(partial(io_data.read, 9), b'')]
+                    ambe72_frames = iter(partial(io_data.read, 9), b'')
                     ambe49_DSD_frames = p.map(convert_to_DSD, ambe72_frames)
-                    # sys.stdout.buffer.write(b''.join(ambe49_DSD_frames))
-                    # sys.stdout.buffer.flush()
                     pipe[1].send_bytes(b''.join(ambe49_DSD_frames))
                     data_buffer = b''
             else:
@@ -62,8 +69,9 @@ def output_process(input_pipe, tones):
         else:
             # when no data, output silent frame
             if time() - start > 0.5:
-                sys.stdout.buffer.write(silent.tobytes())
+                sys.stdout.buffer.write(silent.tobytes()*20)
                 sys.stdout.flush()
+                start = time()
             elif time() - start > 9 * 60:
                 sys.stdout.buffer.write(tones.tobytes())
                 sys.stdout.flush()
